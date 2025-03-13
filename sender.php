10ba70c827a9d64cc8d5a2e3e213a910ed0eb071
Levi <levi.johansen@kreftforeningen.no>
2025-03-13 11:24:45 +0100

    add: create email object and send

--- sender.php
+++ sender.php
@@ -4,6 +4,8 @@
 require_once 'vendor/autoload.php';
 require_once 'config.php';
 
+$sendgrid_template = 'd-520eadfd50f2475a93a08afd9b0eb073';
+
 if ( 1 == count($argv) )
     die('no email file provided');
 
@@ -19,7 +21,25 @@ $emails = array_filter($file_lines, fn($x) => filter_var($x, FILTER_VALIDATE_EMA
 printf("found %d emails in %d lines\n", count($emails), count($file_lines));
 
 
-$sendgrid = new \SendGrid(SENDGRID_API_KEY);
+$mail = new \SendGrid\Mail\Mail();
+$mail->setFrom('send@stafettforlivet.no');
+$mail->setTemplateId($sendgrid_template);
 
-foreach( $emails as $key => $val )
-    print "$key => $val\n";
+$mail_settings = new \SendGrid\Mail\MailSettings();
+$mail_settings->setSandboxMode(true);
+$mail->setMailSettings($mail_settings);
+
+foreach( $emails as $email )
+{
+    $to = new \SendGrid\Mail\To($email);
+    $personalization = new \SendGrid\Mail\Personalization();
+    $personalization->addTo($to);
+    $mail->addPersonalization($personalization);
+}
+
+
+$sendgrid = new \SendGrid(SENDGRID_API_KEY);
+$result = $sendgrid->client->mail()->send()->post($mail);
+
+print_r($result);
+
